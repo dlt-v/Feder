5239f0f24cad09f5da9a07755e336e2fef8c4249
Erik Rößiger <erik@erikszeug.de>
2021-07-18 23:46:57 +0200

    Add basic web-app styling #GA-8

--- src/app-shell.ts
+++ src/app-shell.ts
@@ -1,7 +1,15 @@
-import { customElement, LitElement } from 'lit-element';
+import { css, customElement, LitElement, TemplateResult } from 'lit-element';
 
 @customElement('app-shell')
 export class AppShell extends LitElement
 {
+    public static override styles = css`
+        :host
+        {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        }
+    `;
 
 }
