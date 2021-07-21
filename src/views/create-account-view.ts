77cee2f327f30be05c329be4e0123025c388d7a3
tmm-michalski <tmm.michalski@gmail.com>
2021-07-21 21:32:49 +0200

    Add basic server interaction #GA-14

--- src/views/create-account-view.ts
+++ src/views/create-account-view.ts
@@ -3,11 +3,44 @@ import '../ga-heading';
 import '@material/mwc-textfield';
 import '@material/mwc-button';
 
-import { css, customElement, html, LitElement, TemplateResult } from 'lit-element';
+import { css, customElement, html, LitElement, property, TemplateResult } from 'lit-element';
 
 @customElement('create-account-view')
 export class CreateAccountView extends LitElement
 {
+    @property()
+    userName = '';
+    userMail = '';
+    userPass = '';
+    userSecPass = '';
+
+
+
+    private async createAccount(): Promise<void>
+    {
+        console.log(this.userName);
+        console.log(this.userMail);
+        console.log(this.userPass);
+        console.log(this.userSecPass);
+
+        const user = {
+            name: this.userName,
+            password: this.userPass,
+            mail: this.userMail
+        };
+
+        const res = await fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+
+        console.log( res ? 'hello' : 'bye');
+
+    }
+
     public static override styles = css`
         :host
         {
@@ -41,13 +74,31 @@ export class CreateAccountView extends LitElement
         return html`
             <ga-card class="card">
                 <ga-heading>Create an account</ga-heading>
-                <mwc-textfield class="input" outlined required label="User Name"></mwc-textfield>
-                <mwc-textfield class="mail" outlined required label="Mail"></mwc-textfield>
-                <mwc-textfield class="input" outlined required type="password" label="Password"></mwc-textfield>
-                <mwc-textfield class="input" outlined required type="password" label="Confirm password"></mwc-textfield>
-                <mwc-button id="createAccountButton" class="input" raised label="Submit"></mwc-button>
+                <mwc-textfield .value=${this.userName} @change=${this.updateUserName} class="input" outlined required label="User Name"></mwc-textfield>
+                <mwc-textfield .value=${this.userMail} @change=${this.updateUserMail} class="mail" outlined required label="Mail"></mwc-textfield>
+                <mwc-textfield .value=${this.userPass} @change=${this.updateUserPass} class="input" outlined required type="password" label="Password"></mwc-textfield>
+                <mwc-textfield .value=${this.userSecPass} @change=${this.updateUserSecPass} class="input" outlined required type="password" label="Confirm password"></mwc-textfield>
+                <mwc-button id="createAccountButton" @click=${this.createAccount} class="input" raised label="Submit"></mwc-button>
             </ga-card>
             <mwc-button id="loginButton" label="Already have an account?"></mwc-button>
         `;
     }
+
+    private updateUserName(e: any): void
+    {
+        this.userName = e.srcElement.value;
+    }
+    private updateUserMail(e: any): void
+    {
+        this.userMail = e.srcElement.value;
+    }
+    private updateUserPass(e: any): void
+    {
+        this.userPass = e.srcElement.value;
+    }
+    private updateUserSecPass(e: any): void
+    {
+        this.userSecPass = e.srcElement.value;
+    }
+
 }
